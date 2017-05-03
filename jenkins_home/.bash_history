794317894aa64db0394c97a44cd8a42836effeec
Julien ALBERT <julien.albert@free.fr>
2017-05-03 11:56:22 +0200

    JENKINS_HOME save from sample-spring-boot use

---
+++ jenkins_home/.bash_history
@@ -0,0 +1,14 @@
+ping google.fr
+exit
+ping google.fr
+exit
+mvn --version
+ls /var/jenkins_home/
+cat /var/jenkins_home/jenkins.mvn.GlobalMavenConfig.xml 
+ls -la /var/jenkins_home/tools/
+ls -la /var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/
+ls -la /var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/Mvn3.5.0/
+ls -la /var/jenkins_home/tools/hudson.tasks.Maven_MavenInstallation/Mvn3.5.0/conf/
+sudo 
+/etc/init.d/jenkins restart
+exit
---
+++ jenkins_home/jobs/spring-boot - tests build/cobertura/com/example/DemoApplication.java
@@ -0,0 +1,60 @@
+<tr class="noCover">
+<td class="line"><a name='1'/>1</td>
+<td class="hits"/>
+<td class="code">package&nbsp;com.example;</td>
+</tr>
+<tr class="noCover">
+<td class="line"><a name='2'/>2</td>
+<td class="hits"/>
+<td class="code"></td>
+</tr>
+<tr class="noCover">
+<td class="line"><a name='3'/>3</td>
+<td class="hits"/>
+<td class="code">import&nbsp;org.springframework.boot.SpringApplication;</td>
+</tr>
+<tr class="noCover">
+<td class="line"><a name='4'/>4</td>
+<td class="hits"/>
+<td class="code">import&nbsp;org.springframework.boot.autoconfigure.SpringBootApplication;</td>
+</tr>
+<tr class="noCover">
+<td class="line"><a name='5'/>5</td>
+<td class="hits"/>
+<td class="code"></td>
+</tr>
+<tr class="noCover">
+<td class="line"><a name='6'/>6</td>
+<td class="hits"/>
+<td class="code">@SpringBootApplication</td>
+</tr>
+<tr class="coverFull">
+<td class="line"><a name='7'/>7</td>
+<td class="hits">1</td>
+<td class="code">public&nbsp;class&nbsp;DemoApplication&nbsp;{</td>
+</tr>
+<tr class="noCover">
+<td class="line"><a name='8'/>8</td>
+<td class="hits"/>
+<td class="code"></td>
+</tr>
+<tr class="noCover">
+<td class="line"><a name='9'/>9</td>
+<td class="hits"/>
+<td class="code">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;public&nbsp;static&nbsp;void&nbsp;main(String[]&nbsp;args)&nbsp;{</td>
+</tr>
+<tr class="coverNone">
+<td class="line"><a name='10'/>10</td>
+<td class="hits">0</td>
+<td class="code">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SpringApplication.run(DemoApplication.class,&nbsp;args);</td>
+</tr>
+<tr class="coverNone">
+<td class="line"><a name='11'/>11</td>
+<td class="hits">0</td>
+<td class="code">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;}</td>
+</tr>
+<tr class="noCover">
+<td class="line"><a name='12'/>12</td>
+<td class="hits"/>
+<td class="code">}</td>
+</tr>
